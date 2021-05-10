4c315e2a804e65cf43347dfc0d653249651d5982
Maymaher <mai.maher003@gmail.com>
2021-05-09 22:35:33 -0400

    test

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/students-lab'));
+app.use(express.static('./dist/lab-project'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/students-lab/'}),
+    res.sendFile('index.html', {root: 'dist/lab-project/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
